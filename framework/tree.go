b6f49b4ad096e1cffdec421a26368ece39def0dc
wanglicong <w15083127382@163.com>
2023-08-28 18:10:46 +0800

    tree

--- framework/tree.go
+++ framework/tree.go
@@ -1,5 +1,7 @@
 package framework
 
+import "strings"
+
 // Tree structure
 type Tree struct {
 	root *node //根节点
@@ -11,4 +13,69 @@ type node struct {
 	segment string            // uri中的字符串，代表这个节点表示的路由中某个段的字符串
 	handler ControllerHandler // 代表这个节点中包含的控制器，用于最终加载调用
 	childs  []*node           // 代表这个节点下的子节点
+}
+
+// 判断一个segment是否是通用segment,即以：开头
+func isWildSegment(segment string) bool {
+	return strings.HasPrefix(segment, ":")
+}
+
+// 过滤下一层满足segment规则的子节点
+func (n *node) filterChildNodes(segment string) []*node {
+	if len(n.childs) == 0 {
+		return nil
+	}
+	// 如果segment是通配符，则所有下一层子节点都满足需求
+	if isWildSegment(segment) {
+		return n.childs
+	}
+	nodes := make([]*node, len(n.childs))
+
+	//过滤所有的下一层节点
+	for _, cnode := range n.childs {
+		//如果下一层节点有通配符，则满足需求
+		if isWildSegment(cnode.segment) {
+			nodes = append(nodes, cnode)
+		} else if cnode.segment == segment {
+			//如果下一层子节点没有通配符，但是文本完全匹配，则满足需求
+			nodes = append(nodes, cnode)
+		}
+	}
+	return nodes
+}
+
+// 判断路由是否已经存在在节点的所有子节点树中存在了
+func (n *node) matchNode(uri string) *node {
+	//使用分割符将uri切割为两个部分
+	segments := strings.SplitN(uri, "/", 2)
+	//第一个部分用于匹配下一层子节点
+	segment := segments[0]
+	if !isWildSegment(segment) {
+		segment = strings.ToUpper(segment)
+	}
+	//匹配符合的下一层子节点
+	cnodes := n.filterChildNodes(segment)
+	// 如果当前子节点没有一个符合，那么说明这个uri一定是之前不存在, 直接返回nil
+	if cnodes == nil || len(cnodes) == 0 {
+		return nil
+	}
+	//如果只有一个segment，则是最后一个标记
+	if len(segment) == 1 {
+		// 如果segment已经是最后一个节点，判断这些cnode是否有isLast标志
+		for _, tn := range cnodes {
+			if tn.isLast {
+				return tn
+			}
+		}
+		//都不是最后一个节点
+		return nil
+	}
+	//如果有2个segment，递归每个子节点继续进行查找
+	for _, tn := range cnodes {
+		tnMatch := tn.matchNode(segments[1])
+		if tnMatch != nil {
+			return tnMatch
+		}
+	}
+	return nil
 }
