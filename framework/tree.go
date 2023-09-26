6c7384f99a13e440a6bcb4d3845f1fa5f2f64aec
wanglicong <w15083127382@163.com>
2023-09-26 21:34:31 +0800

    tree

--- framework/tree.go
+++ framework/tree.go
@@ -13,9 +13,9 @@ type Tree struct {
 // node 代表节点
 type node struct {
 	isLast   bool                // 该节点是否能成为一个独立的uri, 是否自身就是一个终极节点
-	segment  string              // uri中的字符串
-	handlers []ControllerHandler // 控制器
-	childs   []*node             // 子节点
+	segment  string              // uri中的字符串，代表这个节点表示的路由中某个段的字符串
+	handlers []ControllerHandler // 代表这个节点中包含的控制器，用于最终加载调用
+	childs   []*node             // 代表这个节点下的子节点
 	parent   *node               //父节点，双向指针
 }
 
