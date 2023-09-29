1ada819e2dc51b79bc234d1e92ac82da8640a259
wanglicong <w15083127382@163.com>
2023-09-29 20:57:28 +0800

    time

--- framework/middleware/timeout.go
+++ framework/middleware/timeout.go
@@ -1,1 +1,43 @@
 package middleware
+
+import (
+	"Hade/framework"
+	"context"
+	"fmt"
+	"log"
+	"time"
+)
+
+func Timeout(d time.Duration) framework.ControllerHandler {
+	return func(c *framework.Context) error {
+		finish := make(chan struct{}, 1)
+		panicChan := make(chan interface{}, 1)
+		// 执行业务逻辑前预操作：初始化超时context
+		durationCtx, cancel := context.WithTimeout(c.BaseContext(), d)
+		defer cancel()
+
+		go func() {
+			defer func() {
+				if p := recover(); p != nil {
+					panicChan <- p
+				}
+			}()
+			// 使用next执行具体的业务逻辑
+			c.Next()
+
+			finish <- struct{}{}
+		}()
+		// 执行业务逻辑后操作
+		select {
+		case p := <-panicChan:
+			c.SetStatus(500).Json("time out")
+			log.Println(p)
+		case <-finish:
+			fmt.Println("finish")
+		case <-durationCtx.Done():
+			c.SetStatus(500).Json("time out")
+		}
+		return nil
+	}
+
+}
