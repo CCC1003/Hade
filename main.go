7c7510e7914624a2fc5e2dd3d3bffde76cda0c2e
wanglicong <w15083127382@163.com>
2023-09-29 21:06:17 +0800

    t

--- main.go
+++ main.go
@@ -32,8 +32,7 @@ func main() {
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	//这里会阻塞当前goroutine等待信号
 	<-quit
-
-	// 调用Server.Shutdown graceful结束
+	// 调用Server.Shutdown 结束
 	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := server.Shutdown(timeoutCtx); err != nil {
