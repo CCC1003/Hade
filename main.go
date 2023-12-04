12d26cdc1db42dba64f2c76b6bd00677939e3232
wanglicong <w15083127382@163.com>
2023-12-04 20:38:19 +0800

    clear

--- main.go
+++ main.go
@@ -3,13 +3,7 @@ package main
 import (
 	"Hade/framework"
 	"Hade/framework/middleware"
-	"context"
-	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 )
 
 func main() {
@@ -22,20 +16,5 @@ func main() {
 		//请求监听地址
 		Addr: ":8888",
 	}
-
-	go func() {
-		server.ListenAndServe()
-	}()
-	//当前的goroutine等待信号量
-	quit := make(chan os.Signal)
-	//监控信号：SIGINT，SYSTERM，SIGQUIT
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	//这里会阻塞当前goroutine等待信号
-	<-quit
-	// 调用Server.Shutdown 结束
-	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := server.Shutdown(timeoutCtx); err != nil {
-		log.Fatal("Server Shutdown:", err)
-	}
+	server.ListenAndServe()
 }
